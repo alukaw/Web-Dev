ae7fde05b535f8b6c37d0e0a4802ba37f9e54e53
Alua <aluak2005@gmail.com>
2026-02-23 16:09:13 +0500

    tutorial: define a route

--- Lab4/Task1/learn-angular/src/app/app.routes.ts
+++ Lab4/Task1/learn-angular/src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { User } from './user/user';
+import { About } from './about/about';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: User
-  },
-  {
-    path: 'user',
-    component: User
-  }
+  { path: '', component: Home },
+  { path: 'user', component: User },
+  { path: 'about', component: About }
 ];

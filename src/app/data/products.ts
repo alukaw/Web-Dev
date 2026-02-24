2f1b968edc9e27e00fe7178984ffd60bbf91ca11
Alua <aluak2005@gmail.com>
2026-02-24 14:58:54 +0500

    Online store catalog

---
+++ src/app/data/products.ts
@@ -0,0 +1,144 @@
+import { Product } from '../models/product.model';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'Прокладки',
+    description: 'Kotex прокладки Ultra Night Duo 14 шт',
+    price: 1521,
+    rating: 4.5,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p78/pf0/25958573.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p78/pf0/25958573.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pb0/pf0/25958575.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pcd/pf0/25958576.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/kotex-prokladki-ultra-night-duo-14-sht-100931591/?c=750000000'
+  },
+  {
+    id: 2,
+    name: 'Крем для тела',
+    description: 'APOLLONIA Purity крем для тела 300 мл',
+    price: 2740,
+    rating: 4.2,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p9e/p28/59679201.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p9e/p28/59679201.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p81/p28/59679202.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p65/p28/59679203.png?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/apollonia-purity-krem-dlja-tela-300-ml-130657736/?c=750000000'
+  },
+  {
+    id: 3,
+    name: 'Гель для душа',
+    description: 'Kera Luxe Hyaluronic Acid гель для душа увлажняющий гель 750 мл',
+    price: 2225,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/peb/pa7/29060465.JPG?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/peb/pa7/29060465.JPG?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p07/pa8/29060466.JPG?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p40/pa8/29060468.JPG?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/kera-luxe-hyaluronic-acid-gel-dlja-dusha-uvlazhnjajuschii-gel-750-ml-135990945/?c=750000000'
+  },
+  {
+    id: 4,
+    name: 'Скраб для тела',
+    description: 'The Act скраб антицеллюлитный кокосовый для тела 300 г',
+    price: 2865,
+    rating: 4.7,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p7a/p76/57974532.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p7a/p76/57974532.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pb3/p76/57974534.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pcf/p76/57974535.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/the-act-skrab-antitselljulitnyi-kokosovyi-dlja-tela-300-g-104104022/?c=750000000'
+  },
+  {
+    id: 5,
+    name: 'Маска для тела',
+    description: 'Sugarlife маска для интимной гигиены Осветляющая для бикини 150 мл',
+    price: 4100,
+    rating: 4.6,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p2a/p28/35713981.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p2a/p28/35713981.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p46/p28/35713982.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p62/p28/35713983.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/sugarlife-maska-dlja-intimnoi-gigieny-osvetljajuschaja-dlja-bikini-150-ml-118410601/?c=750000000'
+  },
+  {
+    id: 6,
+    name: 'Пена для ванны',
+    description: 'Parli Cosmetics Bubble bath Cherry пена для ванны 480 мл',
+    price: 3500,
+    rating: 4.5,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/pc3/p58/32534120.jpeg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/pc3/p58/32534120.jpeg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p2c/p5c/32534121.jpeg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p96/p5f/32534122.jpeg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/parli-cosmetics-bubble-bath-cherry-pena-dlja-vanny-480-ml-132167398/?c=750000000'
+  },
+  {
+    id: 7,
+    name: 'Крем для рук',
+    description: 'DR. KANG Ceramide крем для рук 100 мл',
+    price: 1000,
+    rating: 4.3,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p86/pe6/37764990.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p86/pe6/37764990.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc5/h46/84578196750366.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h81/h71/86239572099102.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/dr-kang-ceramide-krem-dlja-ruk-100-ml-102699865/?c=750000000'
+  },
+  {
+    id: 8,
+    name: 'Антиперспирант',
+    description: 'Deonica Невидимый антиперспирант для женщин 50 мл',
+    price: 869,
+    rating: 4.4,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h79/h44/86535831617566.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h79/h44/86535831617566.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p8a/p03/43674078.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p00/pda/43674283.png?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/deonica-nevidimyi-antiperspirant-dlja-zhenschin-50-ml-101584715/?c=750000000'
+  },
+  {
+    id: 9,
+    name: 'Мочалка для тела',
+    description: 'SPASHOP мочалка для тела, для душа черный 1 шт',
+    price: 1500,
+    rating: 4.2,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hcf/h06/86615664492574.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hcf/h06/86615664492574.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h11/h62/86615664558110.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h22/hde/86615664623646.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/spashop-mochalka-dlja-tela-dlja-dusha-chernyi-1-sht-121777790/?c=750000000'
+  },
+  {
+    id: 10,
+    name: 'Скраб для тела',
+    description: 'MIXIT скраб Coconut Salt для тела 300 мл',
+    price: 3096,
+    rating: 4.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hd8/h26/85558537191454.jpg?format=gallery-medium',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hd8/h26/85558537191454.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/hb7/h65/85558537256990.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pc8/p7e/43598279.png?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/mixit-skrab-coconut-salt-dlja-tela-300-ml-117411541/?c=750000000'
+  },
+];
